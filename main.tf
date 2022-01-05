73bfe666a74ce90c52a2096dca73843545ff6f71
Jacob Taunton <jandrewt82@gmail.com>
2022-01-05 07:59:18 -0800

    adding ws1 and ws2

--- main.tf
+++ main.tf
@@ -15,21 +15,7 @@ provider "tfe" {
 #create a workspace
 resource "tfe_workspace" "my_workspaces" {
   count        = length(var.workspaces)
-  name = var.workspaces[count.index]
-#  name         = "${var.env}-${var.workspaces[count.index]}"
+  name         = var.workspaces[count.index]
   organization = var.org
   tag_names    = ["tfc", "automation"]
 }
-
-/*
-resource "tfe_team" "test" {
-  name         = "maddogs"
-  organization = var.org
-}
-
-resource "tfe_team_access" "test" {
-  access       = "read"
-  team_id      = tfe_team.test.id
-  workspace_id = tfe_workspace.test[count.index]
-}
- */
